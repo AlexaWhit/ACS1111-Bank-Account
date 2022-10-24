78292f97abfcafefbfd60f49eb00b6cce73f9ae5
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2022-10-24 16:07:41 -0600

    created BankAccount class

---
+++ BankAccount.py
@@ -0,0 +1,8 @@
+# Create BankAccount class:
+class BankAccount:
+    def __init__(self, full_name, account_number, balance):
+        self.name = full_name
+        self.account_number = account_number
+        self.balance = balance
+
+    
