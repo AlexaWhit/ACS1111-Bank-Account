1b070860f0eae527ff4f542a398bd16e73453d3f
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2022-10-24 16:29:38 -0600

    wrote outline of functions and first try of print_statement method

--- BankAccount.py
+++ BankAccount.py
@@ -5,4 +5,28 @@ class BankAccount:
         self.account_number = account_number
         self.balance = balance
 
-    
+    # Create deposit method
+    # Adds amount to the balance and prints a message
+    def deposit(amount):
+        pass
+    # Create withdraw method
+    # Subtracts amount to the balance and prints a message
+    # Create if statement where else is insufficient funds with message
+    def withdraw(amount):
+        pass
+
+    # Create get_balance method
+    def get_balance(self):
+        pass
+
+    # Create add_interest method: Adds interest to the user's balance. 
+    def add_interest(self):
+        pass
+        #interest = (self.balance * 0.00083)
+
+    # Create print_statement method which prints a message with account name, account number, and balance
+    def print_statement(self):
+        print(f"{self.name}\nAccount No.: {self.account_number}\nBalance: {self.balance}")
+
+alexa_account = BankAccount("Alexa Whitney", "05546992", 0)
+BankAccount.print_statement(alexa_account)
