f18e0a292a0cec6ba0262c866f86501ae613741b
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2022-10-24 17:03:01 -0600

    added random_account_number function

--- BankAccount.py
+++ BankAccount.py
@@ -1,6 +1,12 @@
 # Need this in order to generate random 8 digit account numbers
 from random import randint
 
+#Create function to randomly assign 8 digit account number
+def random_account_number(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
+
 # Create BankAccount class:
 class BankAccount:
     def __init__(self, full_name, account_number, balance):
@@ -9,21 +15,22 @@ class BankAccount:
         self.balance = balance
 
         balance = 0
-        account_number = randint(8)
+        account_number = print(random_account_number(8))
 
     # Create deposit method
     # Adds amount to the balance and prints a message
-    def deposit(amount):
+    def deposit(self):
         amount = input("Please enter an amount to deposit: $")
-        deposit = (f" {amount} + {BankAccount.self.balance}")
-        print(f"Amount deposited: {amount} new balance: {deposit} ")
+        deposit = (f"{amount} + {self.balance}")
+        new_balance = deposit
+        print(f"Amount deposited: {amount} new balance: {new_balance} ")
 
     # Create withdraw method
     # Subtracts amount to the balance and prints a message
     # Create if statement where else is insufficient funds with message
-    def withdraw(amount):
+    def withdraw(self):
         amount = input("Please enter an amount to withdraw: $")
-        withdraw = (f"{BankAccount.self.balance} - {amount}")
+        withdraw = (f"{self.balance} - {amount}")
         print(f"Amount deposited: {amount} new balance: {withdraw} ")
 
     # Create get_balance method
@@ -41,4 +48,4 @@ class BankAccount:
         print(f"{self.name}\nAccount No.: {self.account_number}\nBalance: {self.balance}")
 
 alexa_account = BankAccount("Alexa Whitney", "05546992", 0)
-BankAccount.print_statement(alexa_account)
+BankAccount.deposit(alexa_account)
