9bcdd47a1c66dbcb45ccdc52719147152ea97172
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2022-10-24 19:08:13 -0600

    get balance method fully functional

--- bank_account.py
+++ bank_account.py
@@ -17,8 +17,8 @@ class BankAccount:
         #starting_balance = 0
         account_number = print(random_account_number(8))
 
-    # Create deposit method
-    # Adds amount to the balance and prints a message
+    # DEPOSIT METHOD #
+    # Adds amount to the balance and prints a message showing the amount deposited and the updated balance
     def deposit(self):
         user_input = input("Please enter an amount to deposit: $")
         amount = float(user_input)
@@ -28,9 +28,9 @@ class BankAccount:
         else:
             print("Please enter a valid monetary amount in $X.XX format.")
 
-    # Create withdraw method
-    # Subtracts amount to the balance and prints a message
-    # Create if statement where else is insufficient funds with message
+    # WITHDRAW METHOD #
+    # Subtracts amount from the current balance and prints a message with withdraw amount and updated current balance
+    # If withdraw amount is > current balance, message re insufficient funds appears
     def withdraw(self):
         user_input = input("Please enter an amount to withdraw: $")
         amount = float(user_input)
@@ -42,9 +42,10 @@ class BankAccount:
         else:
             print("Please enter a valid monetary amount in $X.XX format.")
 
-    # Create get_balance method
+    # GET BALANCE METHOD #
+    # Prints user-friendly message with account balance and also returns the current balance of the account
     def get_balance(self):
-        print(f"Your current balance is: {self.balance}")
+        print(f"Your current balance is: ${self.current_balance}")
         return self.current_balance
 
     # Create add_interest method: Adds interest to the user's balance. 
@@ -59,6 +60,6 @@ class BankAccount:
 alexa_account = BankAccount("Alexa Whitney", "05546992", 0)
 BankAccount.deposit(alexa_account)
 BankAccount.withdraw(alexa_account)
-#BankAccount.get_balance(alexa_account)
+BankAccount.get_balance(alexa_account)
 #BankAccount.add_interest(alexa_account)
 #BankAccount.print_statement(alexa_account)
