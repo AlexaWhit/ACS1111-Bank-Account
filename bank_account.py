aedf6f2491b19ed193711431a35f6d1d5b62b077
AlexaWhitDU <112434021+AlexaWhitDU@users.noreply.github.com>
2022-10-24 18:52:26 -0600

    deposit and withdraw methods fully functional

--- bank_account.py
+++ bank_account.py
@@ -9,38 +9,45 @@ def random_account_number(n):
 
 # Create BankAccount class:
 class BankAccount:
-    def __init__(self, full_name, account_number, balance):
+    def __init__(self, full_name, account_number, current_balance):
         self.name = full_name
         self.account_number = account_number
-        self.balance = balance
+        self.current_balance = current_balance
 
-        balance = 0
+        #starting_balance = 0
         account_number = print(random_account_number(8))
 
     # Create deposit method
     # Adds amount to the balance and prints a message
     def deposit(self):
-        amount = input("Please enter an amount to deposit: $")
-        deposit = (f"{amount} + {self.balance}")
-        new_balance = deposit
-        print(f"Amount deposited: {amount} new balance: {new_balance} ")
+        user_input = input("Please enter an amount to deposit: $")
+        amount = float(user_input)
+        if amount > 0:
+            self.current_balance = self.current_balance + amount 
+            print(f"Amount deposited: ${amount} new balance: ${self.current_balance} ")
+        else:
+            print("Please enter a valid monetary amount in $X.XX format.")
 
     # Create withdraw method
     # Subtracts amount to the balance and prints a message
     # Create if statement where else is insufficient funds with message
     def withdraw(self):
-        amount = input("Please enter an amount to withdraw: $")
-        withdraw = (f"{self.balance} - {amount}")
-        print(f"Amount deposited: {amount} new balance: {withdraw} ")
+        user_input = input("Please enter an amount to withdraw: $")
+        amount = float(user_input)
+        if amount > 0:
+            self.current_balance = self.current_balance - amount 
+            print(f"Amount withdrawn: ${amount} new balance: ${self.current_balance} ")
+        else:
+            print("Please enter a valid monetary amount in $X.XX format.")
 
     # Create get_balance method
     def get_balance(self):
         print(f"Your current balance is: {self.balance}")
-        return BankAccount.self.balance
+        return self.current_balance
 
     # Create add_interest method: Adds interest to the user's balance. 
     def add_interest(self):
-        interest = (self.balance * 0.00083)
+        interest = (self.current_balance * 0.00083)
         print(f"The amount of interest is: ${interest}.")
 
     # Create print_statement method which prints a message with account name, account number, and balance
@@ -49,3 +56,7 @@ class BankAccount:
 
 alexa_account = BankAccount("Alexa Whitney", "05546992", 0)
 BankAccount.deposit(alexa_account)
+BankAccount.withdraw(alexa_account)
+BankAccount.get_balance(alexa_account)
+BankAccount.add_interest(alexa_account)
+BankAccount.print_statement(alexa_account)
